63a7104c181e8e987ba4b00813fa897b600175e8
skrcan <skrcan@hotmail.com>
2014-05-21 13:38:51 -0600

    Learning about variables values and data types

---
+++ Hello World Sample/HelloWorld/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Write("Tell me your name: ");
+            //Create some local variables
+            string name;
+            name = Console.ReadLine();
+
+            Console.WriteLine("Hello " + name);
+
+            int five = 5;
+            int seven = 7;
+            int total;
+            total = five + seven;
+            Console.WriteLine("Five + Seven is " + total);
+
+
+            double price = 10.95;
+            int quantity = 3;
+
+            double totalPrice;
+            totalPrice = price * quantity;
+            Console.Write(price);
+            Console.Write(" times ");
+            Console.Write(quantity);
+            Console.Write(" equal ");
+            Console.WriteLine(totalPrice);
+            Console.WriteLine("Computers do math...");
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ VariablesValues + DataTypes/VariablesValuesAndDataTypes/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablesValuesAndDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Order of operations - arithmatic operators..
+            double result = 5 + 6 * 4 + 1;
+            //              |   |   |   |
+            //              |   \   /   |
+            //              |    ---    |
+            //              |     |     |
+            //              \     /     |
+            //               \   /      |
+            //                ---       |
+            //                 |        |
+            //                 \        /
+            //                  \      /
+            //                    ----
+            Console.WriteLine("the result of the math is " + result);
+
+            // What does casting -really- do??
+            char singleDigit = '5';
+            int simpleNumber = 25;
+            //The result of casting...
+            Console.WriteLine("Casting '5' ...");
+            Console.WriteLine((int)singleDigit);
+            Console.WriteLine("Casting 25 ...");
+            Console.WriteLine((double)simpleNumber);
+
+            // The result of converting...
+            Console.WriteLine("converting '5' ...");
+            Console.WriteLine(Convert.ToInt32(singleDigit));
+            Console.WriteLine("Converting 25 ...");
+                Console.WriteLine(Convert.ToDouble(simpleNumber));
+            //"a;ternate" way of converting using the Parse method
+                Console.WriteLine("Converting '5' using int. parse()");
+            // Console.writeLine(int. Parse(SingleDigit)); // can't "parse" a single char
+                Console.WriteLine(int.Parse(singleDigit.ToString())); //can parse a string...
+                Console.WriteLine("The value in the variables singleDigit is " +
+           singleDigit.ToString());
+                Console.WriteLine("converting '5' after calling . ToString()...");
+                Console.WriteLine(Convert.ToInt32(singleDigit.ToString()));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+
+
+
+
+
+
+
+        }
+    }
+}
